85e0338acfc296757d3cc7b4b4e158bfc63b7348
iggy2k <iggyeaton@gmail.com>
2025-02-26 00:19:29 -0500

    Finally works out of the box locally

--- local_conda_setup.sh
+++ local_conda_setup.sh
@@ -1,11 +1,1 @@
-# conda config --set channel_priority strict
-# conda install -n base conda-libmamba-solver
-# conda config --set solver libmamba
-# yes | conda clean --all
-
-mamba create -n pigeon-pack
-eval "$(mamba shell hook --shell $SHELL)"
-mamba activate
-echo "This can take a really long time. Go get some coffee."
-sleep 5
-yes | mamba env -n pigeon-pack update -v -f "environment.yaml"
+conda env create --name 3-8-10 --file=environment.yaml
