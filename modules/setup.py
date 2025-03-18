eeb8db37a6be8c2970933560dc4c767a764d5854
iggy2k <iggyeaton@gmail.com>
2025-03-18 11:30:59 -0400

    Force numpy version (3)

--- modules/setup.py
+++ modules/setup.py
@@ -11,7 +11,8 @@ def install_dependencies(env_yaml_path="./environment.yaml"):
     try:
         with open(env_yaml_path) as file_handle:
             environment_data = yaml.safe_load(file_handle)
-        _install_pip_package('numpy==1.26.4')
+        _install_pip_package('numpy==1.22.4')
+        _install_pip_package('pandas==1.4.2')
         for dependency in tqdm(environment_data["dependencies"], total=len(environment_data["dependencies"])):
             if isinstance(dependency, dict):  # If it's a pip-specific dependency list
                 for lib in dependency['pip']:
@@ -19,6 +20,8 @@ def install_dependencies(env_yaml_path="./environment.yaml"):
             else:
                 # Fixes ImportError: numpy.core.multiarray failed to import
                 if dependency.split("=")[0] == 'numpy':
+                    pass
+                elif dependency.split("=")[0] == 'pandas':
                     pass
                 else:
                     _install_pip_package(dependency.split("=")[0])
